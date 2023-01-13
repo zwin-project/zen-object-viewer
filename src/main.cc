2d0ad66501c36378d5c2dbe51be0fcca25b6ecfa
shierote <taishi2060@gmail.com>
2023-01-13 12:39:17 +0900

    Add icon

--- src/main.cc
+++ src/main.cc
@@ -24,8 +24,8 @@ int
 main(int argc, char const *argv[])
 {
   Viewer viewer;
-  std::string path = "./assets/default_text.stl";
-  float radius = 0.25;
+  std::string path = ZEN_OBJECT_VIEWER_ASSET_DIR "/default_text.stl";
+  float radius = 0.125;
   glm::mat4 transform =
       glm::rotate(glm::mat4(1), -(float)M_PI / 2, glm::vec3(0, 1, 0));
 
